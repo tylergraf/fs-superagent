3f03fa7519c2e51ff702f0a822d379765fc92a5a
Dan Crews <crewsd@gmail.com>
2013-10-18 15:33:43 -0600

    working. Looking at testing now

---
+++ app.js
@@ -0,0 +1,18 @@
+/*global Buffer:false,clearInterval:false,clearTimeout:false,console:false,exports:false,global:false,module:false,process:false,querystring:false,require:false,setInterval:false,setTimeout:false,__filename:false,__dirname:false */
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/sample', function(req, res, next) {
+  console.log(req.headers);
+  res.send('I DID IT I\'M SO AWESOME');
+});
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log('listening');
+});
