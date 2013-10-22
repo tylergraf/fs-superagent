1806efaad85fffb80251416c8d21c1b69e1e886f
Dan Crews <crewsd@gmail.com>
2013-10-22 14:31:49 -0600

    v1.0.0

---
+++ test/server/fs-superagent.js
@@ -0,0 +1,12 @@
+/*global Buffer:false,clearInterval:false,clearTimeout:false,console:false,exports:false,global:false,module:false,process:false,querystring:false,require:false,setInterval:false,setTimeout:false,__filename:false,__dirname:false,describe:false,it:false,before:false,after:false */
+'use strict';
+
+var expect = require('expect.js');
+
+describe("FS Superagent", function() {
+  describe("TODO: some addl unit testing outside of woodruff :)", function() {
+    it ('should fail', function(done) {
+      expect(true).to.be(false);
+    });
+  });
+});
