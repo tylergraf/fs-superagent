b00675d18034b7775a8938122bafa01d770cb4f3
Dan Crews <crewsd@gmail.com>
2013-10-22 16:59:16 -0600

    Moved sample app fully into sample

--- sample/app.js
+++ sample/app.js
@@ -1,13 +1,14 @@
 /*global Buffer:false,clearInterval:false,clearTimeout:false,console:false,exports:false,global:false,module:false,process:false,querystring:false,require:false,setInterval:false,setTimeout:false,__filename:false,__dirname:false */
 'use strict';
 
-var express = require('express');
+var express = require('express')
+  , path = require('path');
 
 var app = express.createServer();
 
 app.use(express.bodyParser());
-app.use(express.static(__dirname + '/assets'));
-app.use(express.static(__dirname + '/sample'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '../assets')));
 
 app.get('/sample/:type', function(req, res, next) {
   console.log(req.headers);
